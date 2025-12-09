228ad0baa8aad17fad8db2d2663e2509f5bbcd60
Jesus Angel Loza Yataco <lozayataco@gmail.com>
2025-12-08 22:28:20 -0500

    a ver

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,14 +44,15 @@ return [
             'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
-            'google' => [
-                'driver' => 'google',
-                'clientId' => env('GOOGLE_DRIVE_CLIENT_ID'),
-                'clientEmail' => env('GOOGLE_DRIVE_CLIENT_EMAIL'),
-                'credentialsPath' => env('GOOGLE_DRIVE_CREDENTIALS_PATH'),
-                'folderId' => env('GOOGLE_DRIVE_FOLDER_ID'),
-            ],
             'report' => false,
+        ],
+
+        'google' => [
+            'driver' => 'google',
+            'clientId' => env('GOOGLE_DRIVE_CLIENT_ID'),
+            'clientEmail' => env('GOOGLE_DRIVE_CLIENT_EMAIL'),
+            'credentialsPath' => env('GOOGLE_DRIVE_CREDENTIALS_PATH'),
+            'folderId' => env('GOOGLE_DRIVE_FOLDER_ID'),
         ],
 
         's3' => [
